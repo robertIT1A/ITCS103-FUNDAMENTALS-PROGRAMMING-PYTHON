301691bbad16ce409d80483f267959a67bc01bb6
robertIT1A <robertoguevarrajr25@gmail.com>
2026-03-29 20:59:46 +0800

    Add initial Tic Tac Toe GUI implementation

---
+++ GUI/tiktak.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Tiktak")
+
+def pili(target_btn):
+    lagay = tk.Entry(window)
+    if user press x or o
+
+head = tk.Label(window,text="Tic Tac Toe")
+
+frame = tk.Frame(window)
+
+btnA = tk.Button(frame,text="A", command=lambda: pili(btnA))
+btnB = tk.Button(frame,text="B", command=lambda: pili(btnB))
+btnC = tk.Button(frame,text="C", command=lambda: pili(btnC))
+
+btnD = tk.Button(frame,text="D", command=lambda: pili(btnD))
+btnE = tk.Button(frame,text="E", command=lambda: pili(btnE))
+btnF = tk.Button(frame,text="F", command=lambda: pili(btnF))
+
+btnG = tk.Button(frame,text="G", command=lambda: pili(btnG))
+btnH = tk.Button(frame,text="H", command=lambda: pili(btnH))
+btnI = tk.Button(frame,text="I", command=lambda: pili(btnI))
+
+head.grid(row=0,column=0)
+frame.grid(row=1,column=0)
+btnA.grid(row=0,column=0,ipadx=5)
+btnB.grid(row=0,column=1,ipadx=5)
+btnC.grid(row=0,column=2,ipadx=5)
+
+btnE.grid(row=1,column=0,ipadx=5)
+btnD.grid(row=1,column=1,ipadx=5)
+btnF.grid(row=1,column=2,ipadx=5)
+
+btnG.grid(row=2,column=0,ipadx=5)
+btnH.grid(row=2,column=1,ipadx=5)
+btnI.grid(row=2,column=2,ipadx=5)
+
+
+window.mainloop()
