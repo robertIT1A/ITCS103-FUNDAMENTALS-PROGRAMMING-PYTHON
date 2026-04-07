912756feb693ea7ede28e7c04405e0810e3db8ab
robertIT1A <robertoguevarrajr25@gmail.com>
2026-04-07 11:10:01 +0800

    Create file checking script
    Add script to check for file existence and read content

---
+++ Python Handles File/checking.py
@@ -0,0 +1,15 @@
+Import os
+
+current = os. getcwd()
+print(current)
+
+file_path = os.path.join(current, "trial. txt")
+
+
+
+if os.path.exists(file_path):
+     file - open("trial.txt", "r")
+     content - file-read()
+     print(content)
+else:
+     print("File not Found")
