0d6984906fd767cbb1e58f7aa8974b120abf631d
robertIT1A <robertoguevarrajr25@gmail.com>
2026-04-14 20:56:52 +0800

    Update checking.py

--- Python Handles File/checking.py
+++ Python Handles File/checking.py
@@ -3,13 +3,13 @@ import os
 current = os. getcwd()
 print(current)
 
-file_path = os.path.join(current, "trial. txt")
+file_path = os.path.join(current, "trial.txt")
 
 
 
 if os.path.exists(file_path):
-     file - open("trial.txt", "r")
-     content - file-read()
+     file = open("trial.txt", "r")
+     content = file.read()
      print(content)
 else:
      print("File not Found")
