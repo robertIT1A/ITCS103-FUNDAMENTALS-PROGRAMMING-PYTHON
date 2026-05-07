0e3456e7170aac2e3269089c21e32c3aa8c5b89f
robertIT1A <robertoguevarrajr25@gmail.com>
2026-05-07 11:02:49 +0800

    Create AGE_GUI.py for age calculator application

---
+++ tkinter and openpyxl/AGE_GUI.py
@@ -0,0 +1,72 @@
+import tkinter as tk
+import openpyxl as op
+from tkinter import messagebox,ttk
+
+
+def display():
+    workbook = op.load_workbook("excelDB.xlsx")
+    sheet = workbook.active
+
+    for row in tree.get_children():
+        tree.delete(row)
+
+    for row in sheet.iter_rows(min_row=2,values_only=True):
+        tree.insert("",tk.END,values=row)
+
+window=tk.Tk()
+window.title("Age Calculator")
+window.configure(bg="lightgreen")
+
+
+#Form Title
+title = tk.Label ( window, text="Profile Builder", font=("Times New Roman",14,"bold"),bg="lightgreen")
+title.grid(row=0, column=0, columnspan=6)
+
+#Frame
+genframe = tk.Frame(window,bg="lightgreen",bd=2, relief="groove")
+genframe.grid(row=1,column=0, columnspan=6,padx=10,pady=10)
+
+#First Name Entry
+fname_entry = tk.Entry(genframe, font=("Poppins",12))
+fname_entry.grid(row=2, column=1,columnspan=2,padx=(10,0),pady=(10,0))
+
+fname_label = tk.Label(genframe, text="First Name", font=("Poppins",10,"italic"),bg="lightgreen")
+fname_label.grid(row=3, column=1,columnspan=2)
+
+#Middle Name Entry
+mname_entry = tk.Entry(genframe, font=("Poppins",12))
+mname_entry.grid(row=2, column=3,columnspan=2,padx=(10,0),pady=(10,0))
+
+mname_label = tk.Label(genframe, text="Middle Name", font=("Poppins",10,"italic"),bg="lightgreen")
+mname_label.grid(row=3, column=3,columnspan=2)
+
+#Last Name Entry
+lname_entry = tk.Entry(genframe, font=("Poppins",12))
+lname_entry.grid(row=2, column=5,columnspan=2,padx=(10,10),pady=(10,0))
+
+lname_label = tk.Label(genframe, text="Last Name", font=("Poppins",10,"italic"),bg="lightgreen")
+lname_label.grid(row=3, column=5,columnspan=2)
+
+#Birthyear Entry
+birth_entry = tk.Entry(genframe, font=("Poppins",12))
+birth_entry.grid(row=4, column=1,columnspan=2,padx=(10,0))
+
+birthyear_label = tk.Label(genframe, text="Birth Year", font=("Poppins",10,"italic"),bg="lightgreen")
+birthyear_label.grid(row=5, column=2,columnspan=2)
+
+update_btn = tk.Button(window, text="Update")
+update_btn.grid(row=6, column=2)
+
+button= tk.Button(window,text="Submit",font=("Poppins",12,"bold"),bg="lightpink",)
+button.grid(row=6, column=0, columnspan=6,pady=(10,20))
+
+delete_btn = tk.Button(window, text="Delete",  bg="red", fg="white")
+delete_btn.grid(row=6, column=3)
+
+tree = ttk.Treeview(window, columns=("ID","Last","First","Middle","BirthYear","Age"), show="headings")
+for col in ("ID","Last","First","Middle","BirthYear","Age"):
+    tree.heading(col, text=col)
+tree.grid(row=7, column=0, columnspan=4)
+
+display()
+window.mainloop()
